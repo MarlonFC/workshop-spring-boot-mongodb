87543ced2bd72bdc464c9a7711e6ebf66c55939a
Marlon Costa <marlonfcosta90@gmail.com>
2021-01-25 19:45:20 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/MarlonFC/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.MarlonFC.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.MarlonFC.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
--- src/main/java/com/MarlonFC/workshopmongo/resources/UseResource.java
+++ src/main/java/com/MarlonFC/workshopmongo/resources/UseResource.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,20 +12,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.MarlonFC.workshopmongo.domain.User;
+import com.MarlonFC.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UseResource {
+	
+	@Autowired
+	private UserService service;
 
 	// @RequestMapping(method = RequestMethod.GET)
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
 
-		User maria = new User("1", "Maria Brown", "maria@gmail.com");
-		User alex = new User("2", "Alex Green", "alex@gmail.com");
-
-		List<User> list = new ArrayList<User>();
-		list.addAll(Arrays.asList(maria, alex));
+//		User maria = new User("1", "Maria Brown", "maria@gmail.com");
+//		User alex = new User("2", "Alex Green", "alex@gmail.com");
+//		List<User> list = new ArrayList<User>();
+//		list.addAll(Arrays.asList(maria, alex));
+		
+		List<User> list = service.findAll();
 
 		return ResponseEntity.ok().body(list);
 
